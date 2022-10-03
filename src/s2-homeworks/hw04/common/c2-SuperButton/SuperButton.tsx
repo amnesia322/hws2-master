1359c1e97e5fd3c1cc2e5662e704a3053a59eaa4
amnesia322 <dev.amnesia322@gmail.com>
2022-10-03 21:12:57 +0300

    hw4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,11 +18,10 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+         + (disabled ? ' ' +  s.disabled : ' ' + s.default)
+         + (xType === 'red' ? ' ' + s.red : '')
+        + (xType === 'secondary' ? ' ' +  s.secondary : '')
+        + (className ? ' ' + s.className : '') // задачка на смешивание классов
 
     return (
         <button
