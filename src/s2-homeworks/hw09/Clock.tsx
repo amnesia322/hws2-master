d2d2912f47c1345d3fdaa0080b6c52a05a0cf1eb
amnesia322 <dev.amnesia322@gmail.com>
2022-11-04 04:07:15 +0300

    refactoring

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -32,20 +32,24 @@ function Clock() {
         setShow(false)
     }
 
-    const stringTime = new Intl.DateTimeFormat("ru", {
+    const formatter = (locales: string, options: object) => {
+        return new Intl.DateTimeFormat(locales, options).format(date)
+    }
+
+    const stringTime = formatter('ru', {
         hour: "numeric",
         minute: "numeric",
         second: "numeric"
-    }).format(date) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = new Intl.DateTimeFormat("en-US", {
-        day: "numeric",
-        month: "numeric",
-        year: "numeric"
-    }).format(date) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    }) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate =formatter('ru', {
+            day: "numeric",
+            month: "numeric",
+            year: "numeric"
+    }) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = new Intl.DateTimeFormat("en-US", {weekday: "long"}).format(date) || <br/> // пишут студенты
-    const stringMonth = new Intl.DateTimeFormat("en-US", {month: "long"}).format(date) || <br/> // пишут студенты
+    const stringDay =  formatter("en-US", {weekday: "long"}) || <br/> // пишут студенты
+    const stringMonth =formatter("en-US", {month: "long"}) || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
