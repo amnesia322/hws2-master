97efa040ef7f8d49c419a6ff3f5faceee10564eb
amnesia322 <dev.amnesia322@gmail.com>
2022-11-17 14:10:44 +0300

    hw11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
 
-const SuperRange: React.FC<SliderProps> = (props) => {
+const SuperRange: React.FC<any> = (props) => {
+    const onChangeHandler = (event: Event, newValue: number | number[]) => {
+props.onChange(event, newValue)
+    }
+
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
                 
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+            value={props.value}
+            onChange={onChangeHandler}
+            color={'green'}
         />
     )
 }
